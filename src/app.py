39440f0c6b441b9aab99f837aa0b368c4f74c579
proyect1... <williamjoelramosbarriga@gmail.com>
2025-12-11 16:05:43 -0400

    inicio

---
+++ src/app.py
@@ -0,0 +1,4 @@
+import os
+class Calculdora:
+    def sum(self,a:int,b:int) -> int:
+        return 0 #a+b
---
+++ src/test.py
@@ -0,0 +1,5 @@
+from app import Calculdora
+def test_suma_2_numeros():
+    assert Calculdora().sun(2,2) == 4
+#pip isntall pytest
+
